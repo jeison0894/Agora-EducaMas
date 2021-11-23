16f994b613504c55fb237d43da00780c33debeb8
JuanEstevesGirata <juandavid.estevesgirata@gmail.com>
2021-11-23 14:54:12 -0500

    Lógica traer todos los estudiantes realizada

--- src/App.js
+++ src/App.js
@@ -1,56 +1,65 @@
-import axios from "axios";
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import {
   dispatchGetUser,
   dispatchLogin,
-  fetchUser,
-} from "./redux/actions/authAction";
-import Autentification from "./componentes/autentication/Autentification";
-import "./App.css";
-import Unification from "./Unification";
+  fetchUser
+} from './redux/actions/authAction'
+import Autentification from './componentes/autentication/Autentification'
+import './App.css'
+import Unification from './Unification'
+import { dispatchGetAllStudents, fetchAllStudents } from './redux/actions/usersAction'
 
-function App() {
-  const dispatch = useDispatch();
-  const token = useSelector((state) => state.token);
-  const auth = useSelector((state) => state.auth);
+function App () {
+  const dispatch = useDispatch()
+  const token = useSelector(state => state.token)
+  const auth = useSelector(state => state.auth)
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem("loggedAgoraUser");
-    const firstLogin = localStorage.getItem("firstLogin");
+    const loggedUserJSON = window.localStorage.getItem('loggedAgoraUser')
+    const firstLogin = localStorage.getItem('firstLogin')
     if (firstLogin && loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON);
-      const refreshtoken = user.refresh_token;
+      const user = JSON.parse(loggedUserJSON)
+      const refreshtoken = user.refresh_token
 
       const getToken = async () => {
         const res = await axios.post(
-          "http://localhost:3005/api/refresh_token",
+          'http://localhost:3005/api/refresh_token',
           { refreshtoken }
-        );
-        console.log(res);
-        dispatch({ type: "GET_TOKEN", payload: res.data.access_token });
-      };
-      getToken();
+        )
+        console.log(res)
+        dispatch({ type: 'GET_TOKEN', payload: res.data.access_token })
+      }
+      getToken()
     }
-  }, [auth.isLogged, dispatch]);
+  }, [auth.isLogged, dispatch])
 
   useEffect(() => {
     if (token) {
       const getUser = () => {
-        dispatch(dispatchLogin());
-        return fetchUser(token).then((res) => {
-          dispatch(dispatchGetUser(res));
-        });
-      };
-      getUser();
+        dispatch(dispatchLogin())
+        return fetchUser(token).then(res => {
+          dispatch(dispatchGetUser(res))
+        })
+      }
+      getUser()
     }
-  }, [token, dispatch]);
+  }, [token, dispatch])
+
+  useEffect(() => {
+    if (auth.isTeacher) {
+      fetchAllStudents(token).then(res => {
+        dispatch(dispatchGetAllStudents(res))
+      })
+    }
+  }, [token, auth.isTeacher, dispatch])
   return (
     <>
       <Autentification />
       {/* <Unification /> */}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
